265a6b2bde875c9256b6b3ee4b0d2c65614a53f1
siftniloy <sifatniloy18@gmail.com>
2022-03-30 23:09:11 +0600

    selected model cart added

--- src/components/Hired/Hired.js
+++ src/components/Hired/Hired.js
@@ -3,15 +3,23 @@ import './Hired.css'
 
 const Hired = (props) => {
     // console.log(props.cart);
-    const { cart } = props;
+    // const { cart } = props;
 
     return (
         <div className='hired-models'>
             <h1>Selected Models: </h1>
 
             {
-                props.cart.map(a => <h4 className='models'>{a.name}</h4>)
+
+                props.cart.map(a => <div className='selected-profile'><img className='selected-img' src={a.image} alt="" /><h4 className='models'>{a.name}</h4> </div>)
             }
+            {/*  */}
+            <div>
+                {/* */}
+
+            </div>
+
+
         </div>
     );
 };
